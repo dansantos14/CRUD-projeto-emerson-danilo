bf6c7dba617b652da83a2eedb28860a56fa67e4b
Danilo Santos <dansantos14@gmail.com>
2024-01-07 02:24:38 -0300

    CRUD project created

---
+++ src/main/java/br/com/daniloaugustosantos/crud/entities/User.java
@@ -0,0 +1,47 @@
+package br.com.daniloaugustosantos.crud.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PESSOA")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String Name;
+	private String Email;
+	
+	public User() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+}
